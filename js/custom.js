1b994fd82ce0b32ae76295c1e55b4400fd8c666a
alcidesjunior <user.amil>
2016-09-30 15:25:43 -0300

    pre-loader

--- js/custom.js
+++ js/custom.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
 		$(".typed").typewriter({
 			speed: 60
 		});
-	}, 3000);
+	}, 5000);
 	var validateEmail = function(email) {
 	    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 	    return re.test(email);
